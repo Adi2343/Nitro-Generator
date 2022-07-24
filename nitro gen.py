256cd40a9f392d9d382d7053cc3299c1f9c23e2c
Adi2343 <90987075+Adi2343@users.noreply.github.com>
2022-07-24 14:38:38 +0200

    Update nitro gen.py

--- nitro gen.py
+++ nitro gen.py
@@ -2,12 +2,12 @@ import random
 Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
 print("Nitro Generator made by Adi ✞#9566")
 while 1:
-    password_len = 16
-    password_count = int(input("Ile kodów wygenerować: "))
-    for x in range(0, password_count):
-        password = ""
-        for x in range(0, password_len):
-            password_char = random.choice(Chars)
-            password = password + password_char
-        print("https://discord.gift/" + password)
+    nitro_len = 16
+    nitro_count = int(input("Ile kodów wygenerować: "))
+    for x in range(0, nitro_count):
+        nitro = ""
+        for x in range(0, nitro_len):
+            nitro_char = random.choice(Chars)
+            nitro = nitro + nitro_char
+        print("https://discord.gift/" + nitro)
     break
